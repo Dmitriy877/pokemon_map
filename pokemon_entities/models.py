2e1aba742ecea8cb0197813a5e079749d0b68fd6
Dmitriy877 <dmitriybukin05@gmail.com>
2025-04-16 14:25:20 +0300

    Revert "update"
    This reverts commit ea625821da493ab7c9028f3e40b13ea6e63c1462.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,9 +5,8 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    picture = models.ImageField(upload_to="pokemons_photos", blank=True)
-    description = models.TextField(blank=True)
-
+    picture = models.ImageField(upload_to="pokemons_photos", null=True)
+    
     def __str__(self):
         return f"{self.title}"
 
